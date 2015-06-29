2690c4b262cbbfadd424dd667f9847cf17c68ae6
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-06-29 21:56:16 +0200

    Scaffold bash based provisioning

---
+++ provision/provision.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# provision.sh
+#
+# This file is specified in Vagrantfile and is loaded by Vagrant as the primary
+# provisioning script whenever the commands `vagrant up`, `vagrant provision`,
+# or `vagrant reload` are used.
