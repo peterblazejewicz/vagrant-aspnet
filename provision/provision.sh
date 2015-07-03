96e7fd67bada9bfd93384e48b0fe4c499462b28b
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-07-03 23:20:42 +0200

    Provision NodeJS

--- provision/provision.sh
+++ provision/provision.sh
@@ -5,3 +5,14 @@
 # This file is specified in Vagrantfile and is loaded by Vagrant as the primary
 # provisioning script whenever the commands `vagrant up`, `vagrant provision`,
 # or `vagrant reload` are used.
+apt-get update
+# install tools documented by Hackaton starter kit
+sudo apt-get install build-essential
+# Install other build tools
+apt-get install -y make g++ git curl vim libcairo2-dev libav-tools nfs-common portmap
+# Install Node
+apt-get update
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install -y nodejs
+sudo npm update -g npm
+sudo npm install -g nodemon
