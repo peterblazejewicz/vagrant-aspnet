494562bfb20bd80b3de6a63a8ff3c7943be9e5c2
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-07-04 22:12:20 +0200

    Install Libuv library and tools

--- provision/provision.sh
+++ provision/provision.sh
@@ -9,9 +9,19 @@ apt-get update
 # install tools documented by Hackaton starter kit
 sudo apt-get install build-essential
 # Install other build tools
-apt-get install -y make g++ git curl vim libcairo2-dev libav-tools nfs-common portmap
+apt-get install -y make g++ git curl vim libcairo2-dev libav-tools nfs-common portmap automake libtool
 # install mono (4.*)
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 sudo apt-get update
 sudo apt-get install mono-complete
+# install libuv
+sudo apt-get install
+curl -sSL https://github.com/libuv/libuv/archive/v1.4.2.tar.gz | sudo tar zxfv - -C /usr/local/src
+cd /usr/local/src/libuv-1.4.2
+sudo sh autogen.sh
+sudo ./configure
+sudo make
+sudo make install
+sudo rm -rf /usr/local/src/libuv-1.4.2 && cd ~/
+sudo ldconfig
