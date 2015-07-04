12f73b2f33e46063602e41094f4fcfde8bfc3246
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-07-04 22:09:00 +0200

    Install Mono framework

--- provision/provision.sh
+++ provision/provision.sh
@@ -10,3 +10,8 @@ apt-get update
 sudo apt-get install build-essential
 # Install other build tools
 apt-get install -y make g++ git curl vim libcairo2-dev libav-tools nfs-common portmap
+# install mono (4.*)
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+sudo apt-get update
+sudo apt-get install mono-complete
