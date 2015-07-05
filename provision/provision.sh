dfb1a0e75fa878b86c3c54602c52230419a7dcab
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-07-05 21:29:16 +0200

    Update provisioning scripts

--- provision/provision.sh
+++ provision/provision.sh
@@ -4,25 +4,35 @@
 #
 # This file is specified in Vagrantfile and is loaded by Vagrant as the primary
 # provisioning script whenever the commands `vagrant up`, `vagrant provision`,
-# or `vagrant reload` are used.
-apt-get update
-# install tools documented by Hackaton starter kit
-sudo apt-get install build-essential
-# Install other build tools
-apt-get install -y make g++ git curl vim libcairo2-dev libav-tools nfs-common portmap automake libtool unzip
-# install mono (4.*)
+# MIXED implementation sources
+# ENV
+MONO_VERSION=4.0.1
+LIBUV_VERSION=1.4.2
+DNX_VERSION=1.0.0-beta5
+DNX_USER_HOME=~/.dnx
+# RUN
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+echo "deb http://download.mono-project.com/repo/debian wheezy main" \
+  | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 sudo apt-get update
-sudo apt-get install mono-complete
-# install libuv
-curl -sSL https://github.com/libuv/libuv/archive/v1.4.2.tar.gz | sudo tar zxfv - -C /usr/local/src
-cd /usr/local/src/libuv-1.4.2
+sudo apt-get install -y mono-complete
+sudo apt-get install -y automake \
+  libtool \
+  curl \
+  unzip
+curl -sSL https://github.com/libuv/libuv/archive/v$LIBUV_VERSION.tar.gz \
+  | sudo tar zxfv - -C /usr/local/src
+cd /usr/local/src/libuv-$LIBUV_VERSION
 sudo sh autogen.sh
 sudo ./configure
 sudo make
 sudo make install
-sudo rm -rf /usr/local/src/libuv-1.4.2 && cd ~/
+sudo rm -rf /usr/local/src/libuv-$LIBUV_VERSION && cd ~/
 sudo ldconfig
-# install DNVM
-curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
+cd /home/vagrant
+curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh \
+  | DNX_BRANCH=dev sh
+source $DNX_USER_HOME/dnvm/dnvm.sh \
+  && dnvm install $DNX_VERSION -a default \
+  && dnvm alias default | xargs -i ln -s $DNX_USER_HOME/runtimes/{} $DNX_USER_HOME/runtimes/default
+export PATH=$PATH:$DNX_USER_HOME/runtimes/default/bin
